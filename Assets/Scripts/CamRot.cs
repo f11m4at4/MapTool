9756ad8c1b93b2d2962b3d99476570cb3a87e83c
f11m4at4 <f11m4at4@gmail.com>
2021-02-02 12:07:08 +0900

    타워디펜스 샘플 적용

---
+++ Assets/Scripts/CamRot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRot : MonoBehaviour
+{
+    Vector3 angles;
+    public float rotSpeed = 200;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 처음 카메라 각도 적용 해주기위해 저장
+        angles = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = -Input.GetAxis("Mouse Y");
+        float y = Input.GetAxis("Mouse X");
+
+        angles.x += x * rotSpeed * Time.deltaTime;
+        angles.y += y * rotSpeed * Time.deltaTime;
+
+        angles.x = Mathf.Clamp(angles.x, -90, 90);
+
+        transform.eulerAngles = angles;
+    }
+}
---
+++ Assets/Scripts/Drone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Drone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform target = GameObject.Find("Player").transform;
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
